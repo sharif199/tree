78c731b3705fb74028223503a1d586430c4dd98c
David Diederich <d.diederich@opengroup.org>
2020-03-18 01:19:54 -0400

    Adding environment variable lookups for property defaults

--- testing/storage-test-aws/src/test/java/org/opengroup/osdu/storage/util/AWSCognitoClient.java
+++ testing/storage-test-aws/src/test/java/org/opengroup/osdu/storage/util/AWSCognitoClient.java
@@ -9,11 +9,12 @@ import java.util.Map;
 
 public class AWSCognitoClient {
     public static String getTokenForUserWithAccess(){
-        String clientId = System.getProperty("AWS_COGNITO_CLIENT_ID");
-        String authFlow = System.getProperty("AWS_COGNITO_AUTH_FLOW");
+        String clientId = System.getProperty("AWS_COGNITO_CLIENT_ID", System.getenv("AWS_COGNITO_CLIENT_ID"));
+        String authFlow = System.getProperty("AWS_COGNITO_AUTH_FLOW",  System.getenv("AWS_COGNITO_AUTH_FLOW"));
+
         Map<String, String> authParameters = new HashMap<>();
-        authParameters.put("USERNAME", System.getProperty("AWS_COGNITO_AUTH_PARAMS_USER"));
-        authParameters.put("PASSWORD", System.getProperty("AWS_COGNITO_AUTH_PARAMS_PASSWORD"));
+        authParameters.put("USERNAME", System.getProperty("AWS_COGNITO_AUTH_PARAMS_USER", System.getenv("AWS_COGNITO_AUTH_PARAMS_USER")));
+        authParameters.put("PASSWORD", System.getProperty("AWS_COGNITO_AUTH_PARAMS_PASSWORD", System.getenv("AWS_COGNITO_AUTH_PARAMS_PASSWORD")));
 
         AWSCognitoIdentityProvider provider = AWSCognitoBuilder.generateCognitoClient();
         InitiateAuthRequest request = new InitiateAuthRequest();
@@ -26,11 +27,11 @@ public class AWSCognitoClient {
     }
 
     public static String getTokenForUserWithNoAccess(){
-        String clientId = System.getProperty("AWS_COGNITO_CLIENT_ID");
-        String authFlow = System.getProperty("AWS_COGNITO_AUTH_FLOW");
+        String clientId = System.getProperty("AWS_COGNITO_CLIENT_ID", System.getenv("AWS_COGNITO_CLIENT_ID"));
+        String authFlow = System.getProperty("AWS_COGNITO_AUTH_FLOW", System.getenv("AWS_COGNITO_AUTH_FLOW"));
         Map<String, String> authParameters = new HashMap<>();
-        authParameters.put("USERNAME", System.getProperty("AWS_COGNITO_AUTH_PARAMS_USER_NO_ACCESS"));
-        authParameters.put("PASSWORD", System.getProperty("AWS_COGNITO_AUTH_PARAMS_PASSWORD"));
+        authParameters.put("USERNAME", System.getProperty("AWS_COGNITO_AUTH_PARAMS_USER_NO_ACCESS", System.getenv("AWS_COGNITO_AUTH_PARAMS_USER_NO_ACCESS")));
+        authParameters.put("PASSWORD", System.getProperty("AWS_COGNITO_AUTH_PARAMS_PASSWORD", System.getenv("AWS_COGNITO_AUTH_PARAMS_PASSWORD")));
 
         AWSCognitoIdentityProvider provider = AWSCognitoBuilder.generateCognitoClient();
         InitiateAuthRequest request = new InitiateAuthRequest();
