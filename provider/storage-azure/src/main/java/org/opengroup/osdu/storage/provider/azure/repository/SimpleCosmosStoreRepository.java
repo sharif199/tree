614907984fa31879c2e10f9b23ea5b497daaeb64
Erik Leckner <erik.leckner@wipro.com>
2020-09-18 19:41:56 -0700

    refactor

--- provider/storage-azure/src/main/java/org/opengroup/osdu/storage/provider/azure/repository/SimpleCosmosStoreRepository.java
+++ provider/storage-azure/src/main/java/org/opengroup/osdu/storage/provider/azure/repository/SimpleCosmosStoreRepository.java
@@ -16,11 +16,12 @@ package org.opengroup.osdu.storage.provider.azure.repository;
 
 import com.azure.cosmos.FeedOptions;
 import com.azure.cosmos.SqlQuerySpec;
-import org.opengroup.osdu.azure.CosmosStore;
-import org.opengroup.osdu.storage.provider.azure.repository.interfaces.IRepository;
+import com.microsoft.azure.spring.data.cosmosdb.core.query.Criteria;
+import com.microsoft.azure.spring.data.cosmosdb.core.query.CriteriaType;
+import com.microsoft.azure.spring.data.cosmosdb.core.query.DocumentQuery;
+import org.opengroup.osdu.storage.provider.azure.repository.interfaces.CosmosStoreRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.lang.NonNull;
@@ -30,7 +31,7 @@ import org.springframework.util.StringUtils;
 import java.util.*;
 
 
-public class CosmosStoreRepository<T> implements IRepository<T> {
+public class SimpleCosmosStoreRepository<T> implements CosmosStoreRepository<T> {
 
     private static final String ID_MUST_NOT_BE_NULL = "id must not be null";
     private static final String ENTITY_MUST_NOT_BE_NULL = "entity must not be null";
@@ -40,9 +41,9 @@ public class CosmosStoreRepository<T> implements IRepository<T> {
     private final Class<T> domainClass;
 
     @Autowired
-    private ExtendedCosmosStore operation;
+    private CosmosStore operation;
 
-    public CosmosStoreRepository(Class<T> domainClass) {
+    public SimpleCosmosStoreRepository(Class<T> domainClass) {
          this.domainClass = domainClass;
     }
 
@@ -80,15 +81,15 @@ public class CosmosStoreRepository<T> implements IRepository<T> {
     }
 
     public Page<T> findAllItemsAsyncPage(String dataPartitionId, String cosmosDBName, String collection, short pageSize, int pageNum) {
-        return  this.operation.findAllItemsAsyncPage(dataPartitionId, cosmosDBName, collection, this.getDomainClass(), pageSize, pageNum);
+        return this.operation.findAllItemsAsyncPage(dataPartitionId, cosmosDBName, collection, this.getDomainClass(), pageSize, pageNum);
     }
 
     public List<T> queryItemsAsync(String dataPartitionId, String cosmosDBName, String collection, SqlQuerySpec query, short pageSize, int pageNum) {
-        return  this.operation.queryItemsAsync(dataPartitionId, cosmosDBName, collection, query, this.getDomainClass(), pageSize, pageNum);
+        return this.operation.queryItemsAsync(dataPartitionId, cosmosDBName, collection, query, this.getDomainClass(), pageSize, pageNum);
     }
 
     public Page<T> queryItemsAsyncPage(String dataPartitionId, String cosmosDBName, String collection, SqlQuerySpec query, short pageSize, int pageNum) {
-        return  this.operation.queryItemsAsyncPage(dataPartitionId, cosmosDBName, collection, query, this.getDomainClass(), pageSize, pageNum);
+        return this.operation.queryItemsAsyncPage(dataPartitionId, cosmosDBName, collection, query, this.getDomainClass(), pageSize, pageNum);
     }
 
     public void upsertItem(String dataPartitionId, String cosmosDBName, String collection, @NonNull T item) {
@@ -96,54 +97,120 @@ public class CosmosStoreRepository<T> implements IRepository<T> {
         this.operation.upsertItem(dataPartitionId, cosmosDBName, collection, item);
     }
 
-    // End CosmosStore methods
-
-    // Start Spring data repository like methods
-
-    public synchronized T getOne(String dataPartitionId, String cosmosDBName, String collection, @NonNull String id, String partitionKey) {
-        Assert.notNull(id, ID_MUST_NOT_BE_NULL);
-        Optional<T> doc = this.findItem(dataPartitionId, cosmosDBName, collection, id, partitionKey);
-        if (!doc.isPresent())
-            return null;
-        return doc.get();
+    @Override
+    public void createItem(String dataPartitionId, String cosmosDBName, String collection, T item) {
+        Assert.notNull(item, ENTITY_MUST_NOT_BE_NULL);
+        this.operation.createItem(dataPartitionId, cosmosDBName, collection, item);
     }
 
-    public T save(String dataPartitionId, String cosmosDBName, String collection, T entity) {
+    // End CosmosStore methods
+
+    // Standard Spring Data Repository
+    /*
+    @Override
+    public Optional<T> findById(String id, String partitionKey) {
+        return Optional.empty();
+    }
+    1
+    @Override
+    public void deleteById(String id, String partitionKey) {
+
+    }
+    2
+    @Override
+    public Iterable<T> findAll(Sort var1) {
+        return null;
+    }
+    3
+    @Override
+    public Page<T> findAll(Pageable var1) {
+        return null;
+    }
+    4
+    @Override
+    public <T> T save(T entity) {
+        return null;
+    }
+    5
+    @Override
+    public <T> Iterable<T> saveAll(Iterable<T> entities) {
+        return null;
+    }
+    6
+    @Override
+    public Optional<T> findById(String id) {
+        return Optional.empty();
+    }
+    7
+    @Override
+    public boolean existsById(String id) {
+        return false;
+    }
+    9
+    @Override
+    public Iterable<T> findAll() {
+        return null;
+    }
+
+    @Override
+    public Iterable<T> findAllById(Iterable<String> ids) {
+        return null;
+    }
+    X'd
+    @Override
+    public long count() {
+        return 0;
+    }
+    8
+    @Override
+    public void deleteById(String id) {
+    }
+    */
+    // Start Spring data repository like methods
+
+    @Override
+    public T save(T entity, String dataPartitionId, String cosmosDBName, String collection) {
         Assert.notNull(entity, ENTITY_MUST_NOT_BE_NULL);
         this.upsertItem(dataPartitionId, cosmosDBName, collection, entity);
         return entity;
     }
 
+    @Override
     public Iterable<T> saveAll(@NonNull Iterable<T> entities, String dataPartitionId, String cosmosDBName, String collection) {
         Assert.notNull(entities, "Iterable entities should not be null");
         List<T> savedEntities = new ArrayList();
         entities.forEach((entity) -> {
-            T savedEntity = this.save(dataPartitionId, cosmosDBName, collection, entity);
+            T savedEntity = this.save(entity, dataPartitionId, cosmosDBName, collection);
             savedEntities.add(savedEntity);
         });
         return savedEntities;
     }
 
+    @Override
     public Iterable<T> findAll(String dataPartitionId, String cosmosDBName, String collection) {
         return this.findAllItems(dataPartitionId, cosmosDBName, collection);
     }
 
+    @Override
     public Optional<T> findById(@NonNull String id, String dataPartitionId, String cosmosDBName, String collection, String partitionKey) {
         Assert.notNull(id, "id must not be null");
         return !StringUtils.hasText(id) ? Optional.empty() : this.findItem(dataPartitionId,  cosmosDBName,  collection,  id,  partitionKey);
     }
 
+    @Override
     public void deleteById(@NonNull String id, String dataPartitionId, String cosmosDBName, String collection, String partitionKey) {
         Assert.notNull(id, ID_MUST_NOT_BE_NULL);
         this.deleteItem(dataPartitionId, cosmosDBName, collection, id, partitionKey);
     }
 
+    @Override
     public boolean existsById(@NonNull String primaryKey, String dataPartitionId, String cosmosDBName, String collection, String partitionKey) {
         Assert.notNull(primaryKey, "primaryKey should not be null");
         return this.exists(primaryKey, dataPartitionId, cosmosDBName, collection, partitionKey);
     }
 
-    public Iterable<T> findAll(@NonNull Sort sort, String dataPartitionId, String cosmosDBName, String collection, FeedOptions options) {
+    @Override
+    public Iterable<T> findAll(@NonNull Sort sort, String dataPartitionId, String cosmosDBName, String collection) {
         Assert.notNull(sort, "sort of findAll should not be null");
         SqlQuerySpec query = new SqlQuerySpec();
         StringBuilder sb = new StringBuilder("SELECT * FROM c");
@@ -158,13 +225,27 @@ public class CosmosStoreRepository<T> implements IRepository<T> {
             }
         }
         query.setQueryText(sb.toString());
+        FeedOptions options = new FeedOptions().setEnableCrossPartitionQuery(true);
         return this.queryItems(dataPartitionId, cosmosDBName, collection, query, options);
     }
 
+    @Override
+    public List<T> findAllById(Iterable<String> ids, String dataPartitionId, String cosmosDBName, String collection, String partitionKey) {
+        Assert.notNull(ids, "Iterable ids should not be null");
+        return this.findByIds(ids, dataPartitionId,  cosmosDBName,  collection,  partitionKey);
+    }
+
+    @Override
+    public List<T> findByIds(Iterable<String> ids, String dataPartitionId, String cosmosDBName, String collection, String partitionKey) {
+        Assert.notNull(ids, "Id list should not be null");
+        DocumentQuery documentQuery = new DocumentQuery(Criteria.getInstance(CriteriaType.IN, "id", Collections.singletonList(ids)));
+        SqlQuerySpec query = new SqlQuerySpec(documentQuery.toString());
+        return this.find(dataPartitionId, cosmosDBName,  collection, query);
+    }
+
+    @Override
     public Page<T> findAll(@NonNull Pageable pageable, String dataPartitionId, String cosmosDBName, String collection) {
-        if (requirePagination) {
-            paginationRequired(pageable);
-        }
+        Assert.notNull(pageable, PAGEABLE_MUST_NOT_BE_NULL);
         //else {
         //    if (isUnpaged(pageable)) {
         //        return new PageImpl<T>((List<T>) this.findAll(dataPartitionId, cosmosDBName, collection));
@@ -176,7 +257,8 @@ public class CosmosStoreRepository<T> implements IRepository<T> {
         return this.findAllItemsAsyncPage(dataPartitionId, cosmosDBName, collection, pageSize, pageNum);
     }
 
-    public Page<T> findAll(@NonNull Pageable pageable, @NonNull Sort sort, String dataPartitionId, String cosmosDBName, String collection, FeedOptions options) {
+    @Override
+    public Page<T> findAll(@NonNull Pageable pageable, @NonNull Sort sort, String dataPartitionId, String cosmosDBName, String collection) {
         Assert.notNull(sort, "sort of findAll should not be null");
         SqlQuerySpec query = new SqlQuerySpec();
         StringBuilder sb = new StringBuilder("SELECT * FROM c");
@@ -196,10 +278,9 @@ public class CosmosStoreRepository<T> implements IRepository<T> {
         return this.queryItemsAsyncPage(dataPartitionId, cosmosDBName, collection, query, (short) pageSize, pageNum);
     }
 
-    public Page<T> query(@NonNull Pageable pageable, String dataPartitionId, String cosmosDBName, String collection, SqlQuerySpec query) {
-        if (requirePagination) {
-            paginationRequired(pageable);
-        }
+    @Override
+    public Page<T> find(@NonNull Pageable pageable, String dataPartitionId, String cosmosDBName, String collection, SqlQuerySpec query) {
+        Assert.notNull(pageable, PAGEABLE_MUST_NOT_BE_NULL);
         //else {
         //    if (isUnpaged(pageable)) {
         //        return new PageImpl<T>((List<T>) this.findAll(dataPartitionId, cosmosDBName, collection));
@@ -208,6 +289,21 @@ public class CosmosStoreRepository<T> implements IRepository<T> {
         int pageNum = pageable.getPageNumber();
         short pageSize = (short) pageable.getPageSize();
         return this.queryItemsAsyncPage(dataPartitionId, cosmosDBName, collection, query, pageSize, pageNum);
+    }
+
+    @Override
+    public List<T> find(String dataPartitionId, String cosmosDBName, String collection, SqlQuerySpec query) {
+        FeedOptions options = new FeedOptions().setEnableCrossPartitionQuery(true);
+        return this.queryItems(dataPartitionId, cosmosDBName, collection, query, options);
+    }
+
+    @Override
+    public synchronized T getOne(@NonNull String id, String dataPartitionId, String cosmosDBName, String collection, String partitionKey) {
+        Assert.notNull(id, ID_MUST_NOT_BE_NULL);
+        Optional<T> doc = this.findItem(dataPartitionId, cosmosDBName, collection, id, partitionKey);
+        if (!doc.isPresent())
+            return null;
+        return doc.get();
     }
 
     private static List<Sort.Order> toOrders(Sort sort) {
@@ -225,13 +321,5 @@ public class CosmosStoreRepository<T> implements IRepository<T> {
         return pageable.isUnpaged();
     }
 
-    private static void paginationRequired(Pageable pageable) {
-        Assert.notNull(pageable, PAGEABLE_MUST_NOT_BE_NULL);
-    }
 
-    // End Spring data repository like methods
-
-    // Start OSDU specific methods
-
-    // End OSDU specific methods
 }
---
+++ provider/storage-azure/src/main/java/org/opengroup/osdu/storage/provider/azure/repository/interfaces/PagingAndSortingRepository.java
@@ -0,0 +1,18 @@
+package org.opengroup.osdu.storage.provider.azure.repository.interfaces;
+
+import com.azure.cosmos.SqlQuerySpec;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.lang.NonNull;
+
+public interface PagingAndSortingRepository<T>  extends CrudRepository<T> {
+    /*
+    Iterable<T> findAll(Sort sort);
+    Page<T> findAll(Pageable pageable);
+    */
+    Page<T> findAll(@NonNull Pageable pageable, String dataPartitionId, String cosmosDBName, String collection);
+    Page<T> findAll(@NonNull Pageable pageable, @NonNull Sort sort, String dataPartitionId, String cosmosDBName, String collection);
+    Iterable<T> findAll(@NonNull Sort sort, String dataPartitionId, String cosmosDBName, String collection);
+    Page<T> find(@NonNull Pageable pageable, String dataPartitionId, String cosmosDBName, String collection, SqlQuerySpec query);
+}
