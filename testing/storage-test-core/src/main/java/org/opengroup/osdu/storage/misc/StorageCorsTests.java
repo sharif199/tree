1cc37c298e5a58d15d0ba23f2e6effe3e1a43b24
Spencer Sutton <suttonsp@amazon.com>
2021-02-05 22:30:08 +0000

    CORS fix
    commit b036c1ca 
    Author: Sutton <suttonsp@147dda3a90de.ant.amazon.com> 
    Date: Fri Feb 05 2021 15:49:37 GMT-0600 (Central Standard Time) 
    
        Latest core lib aws
    
    
    commit 713b54a9 
    Author: Sutton <suttonsp@147dda3a90de.ant.amazon.com> 
    Date: Fri Feb 05 2021 13:04:04 GMT-0600 (Central Standard Time) 
    
        Updating int test to match correct header values for CORS
    
    
    commit 8866142c 
    Author: Sutton <suttonsp@147dda3a90de.ant.amazon.com> 
    Date: Wed Feb 03 2021 16:32:31 GMT-0600 (Central Standard Time) 
    
        Merge branch 'dev' of codecommit::us-east-1://default@os-storage into bug/CORS
    
    
    commit b1100749 
    Author: Sutton <suttonsp@147dda3a90de.ant.amazon.com> 
    Date: Wed Feb 03 2021 16:26:47 GMT-0600 (Central Standard Time) 
    
        core lib update
    
    
    commit 140e9e61 
    Author: Sutton <suttonsp@147dda3a90de.ant.amazon.com> 
    Date: Mon Feb 01 2021 10:25:01 GMT-0600 (Central Standard Time) 
    
        Updating test
    
    
    commit be4e3a55 
    Author: Sutton <suttonsp@147dda3a90de.ant.amazon.com> 
    Date: Mon Feb 01 2021 10:13:11 GMT-0600 (Central Standard Time) 
    
        Merging
    
    
    commit 92cbced3 
    Author: Sutton <suttonsp@147dda3a90de.ant.amazon.com> 
    Date: Mon Feb 01 2021 09:55:21 GMT-0600 (Central Standard Time) 
    
        Had syntax error
    
    
    commit 24846535 
    Author: Sutton <suttonsp@147dda3a90de.ant.amazon.com> 
    Date: Mon Feb 01 2021 09:38:12 GMT-0600 (Central Standard Time) 
    
        Testing cors change from new core common package
    
    
    commit f8bb8342 
    Author: Sutton <suttonsp@147dda3a90de.ant.amazon.com> 
    Date: Wed Jan 27 2021 15:04:23 GMT-0600 (Central Standard Time) 
    
        Should now be able to get core common snapshot
    
    
    commit 1e27856e 
    Author: Sutton <suttonsp@147dda3a90de.ant.amazon.com> 
    Date: Wed Jan 27 2021 13:06:56 GMT-0600 (Central Standard Time) 
    
        Testing response headers factory from gitlab os core common
    
    
    commit 438ec875 
    Author: Sutton <suttonsp@147dda3a90de.ant.amazon.com> 
    Date: Thu Jan 21 2021 09:07:28 GMT-0600 (Central Standard Time) 
    
        Adding response headers factory
    
    
    commit 93136a21 
    Author: Bill Wang <wanzhiji@amazon.com> 
    Date: Tue Jan 05 2021 09:25:57 GMT-0600 (Central Standard Time) 
    
        Squashed commit of the following 
    
    commit cc27772c 
    Author: zhijie wang <wanzhiji@amazon.com> 
    Date: Wed Dec 30 2020 09:18:16 GMT-0800 (Pacific Standard Time) 
    
        remove extra log
    
    commit 102e3135 
    Author: zhijie wang <wanzhiji@amazon.com> 
    Date: Tue Dec 29 2020 16:34:04 GMT-0800 (Pacific Standard Time) 
    
        fix logger location
    
    
    commit 620b19b9 
    Author: zhijie wang <wanzhiji@amazon.com> 
    Date: Tue Dec 29 2020 11:24:07 GMT-0800 (Pacific Standard Time) 
    
        fix test util CognitoBuilder
    
    
    
    commit 4d88e245 
    Author: wanzhiji <wanzhiji@amazon.com> 
    Date: Tue Dec 22 2020 10:45:05 GMT-0600 (Central Standard Time) 
    
        Squashed commit of the following 
    
    commit b0e2af3a 
    Author: zhijie wang <wanzhiji@amazon.com> 
    Date: Mon Dec 21 2020 10:54:09 GMT-0800 (Pacific Standard Time) 
    
        use release, not sanapshot
    
    
    commit 50607488 
    Author: zhijie wang <wanzhiji@amazon.com> 
    Date: Mon Dec 21 2020 08:50:10 GMT-0800 (Pacific Standard Time) 
    
        bump maven version for multi-region integration test

--- testing/storage-test-core/src/main/java/org/opengroup/osdu/storage/misc/StorageCorsTests.java
+++ testing/storage-test-core/src/main/java/org/opengroup/osdu/storage/misc/StorageCorsTests.java
@@ -28,6 +28,8 @@ import org.opengroup.osdu.storage.util.TestBase;
 import org.opengroup.osdu.storage.util.TestUtils;
 import com.sun.jersey.api.client.ClientResponse;
 
+import java.util.Map;
+
 public abstract class StorageCorsTests extends TestBase {
 
     @Test
@@ -37,20 +39,20 @@ public abstract class StorageCorsTests extends TestBase {
 
         MultivaluedMap<String, String> headers = response.getHeaders();
 
-        assertEquals("[*]", headers.get("Access-Control-Allow-Origin").get(0));
+        assertEquals("*", headers.get("Access-Control-Allow-Origin").get(0));
         assertEquals(
-                "[origin, content-type, accept, authorization, data-partition-id, correlation-id, appkey]",
+                "origin, content-type, accept, authorization, data-partition-id, correlation-id, appkey",
                 headers.get("Access-Control-Allow-Headers").get(0));
-        assertEquals("[GET, POST, PUT, DELETE, OPTIONS, HEAD, PATCH]",
+        assertEquals("GET, POST, PUT, DELETE, OPTIONS, HEAD, PATCH",
                 headers.get("Access-Control-Allow-Methods").get(0));
-        assertEquals("[true]", headers.get("Access-Control-Allow-Credentials").get(0));
+        assertEquals("true", headers.get("Access-Control-Allow-Credentials").get(0));
         assertEquals("DENY", headers.get("X-Frame-Options").get(0));
         assertEquals("1; mode=block", headers.get("X-XSS-Protection").get(0));
         assertEquals("nosniff", headers.get("X-Content-Type-Options").get(0));
-        assertEquals("[no-cache, no-store, must-revalidate]", headers.get("Cache-Control").get(0));
-        assertEquals("[default-src 'self']", headers.get("Content-Security-Policy").get(0));
-        assertEquals("[max-age=31536000; includeSubDomains]", headers.get("Strict-Transport-Security").get(0));
-        assertEquals("[0]", headers.get("Expires").get(0));
+        assertEquals("no-cache, no-store, must-revalidate", headers.get("Cache-Control").get(0));
+        assertEquals("default-src 'self'", headers.get("Content-Security-Policy").get(0));
+        assertEquals("max-age=31536000; includeSubDomains", headers.get("Strict-Transport-Security").get(0));
+        assertEquals("0", headers.get("Expires").get(0));
         assertNotNull(headers.get("correlation-id").get(0));
     }
 }
