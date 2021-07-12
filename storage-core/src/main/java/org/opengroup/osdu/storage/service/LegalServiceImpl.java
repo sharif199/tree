97c924d03cea86367f314d65f803c0986ba96624
Mina Otgonbold <AOtgonbold@slb.com>
2021-07-12 09:17:41 -0500

    took out try-catch outside of for-loop

--- storage-core/src/main/java/org/opengroup/osdu/storage/service/LegalServiceImpl.java
+++ storage-core/src/main/java/org/opengroup/osdu/storage/service/LegalServiceImpl.java
@@ -142,27 +142,27 @@ public class LegalServiceImpl implements ILegalService {
     }
 
     private boolean isInCache(Set<String> legalTagNames) {
-        for (String legalTagName : legalTagNames) {
-            String legalTag = null;
-            try {
+        try {
+            for (String legalTagName : legalTagNames) {
+                String legalTag = null;
                 legalTag = this.cache.get(legalTagName);
-            } catch (RedisException ex) {
-                this.log.error(String.format("Error getting key %s from redis: %s", legalTagName, ex.getMessage()), ex);
+                if (legalTag == null) {
+                    return false;
+                }
             }
-            if (legalTag == null) {
-                return false;
-            }
+        } catch (RedisException ex) {
+            this.log.error(String.format("Error getting key %s from redis: %s", legalTagName, ex.getMessage()), ex);
         }
         return true;
     }
 
     private void addToCache(Set<String> legalTagNames) {
-        for (String legalTagName : legalTagNames) {
-            try {
+        try {
+            for (String legalTagName : legalTagNames) {
                 this.cache.put(legalTagName, "Valid LegalTag");
-            } catch (RedisException ex) {
-                this.log.error(String.format("Error putting key %s into redis: %s", legalTagName, ex.getMessage()), ex);
             }
+        } catch (RedisException ex) {
+            this.log.error(String.format("Error putting key %s into redis: %s", legalTagName, ex.getMessage()), ex);
         }
     }
 }
