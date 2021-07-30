e4240d80038ec2d5829f1f736e39036910ab3541
Ronak Sakhuja <rosakhuj@microsoft.com>
2021-07-30 07:32:42 +0000

    Update HealthCheckApi.java

--- storage-core/src/main/java/org/opengroup/osdu/storage/api/HealthCheckApi.java
+++ storage-core/src/main/java/org/opengroup/osdu/storage/api/HealthCheckApi.java
@@ -2,14 +2,20 @@ package org.opengroup.osdu.storage.api;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.opengroup.osdu.core.common.model.storage.StorageRole;
 
 @RestController
 public class HealthCheckApi {
 
     @GetMapping("/health")
-    @PreAuthorize("@authorizationFilter.hasRole('" + StorageRole.ADMIN + "')")
     public String healthMessage() {
         return "Alive";
     }
 
+    @GetMapping("/healthh")
+    @PreAuthorize("@authorizationFilter.hasRole('" + StorageRole.ADMIN + "')")
+    public String healthMessage() {
+        return "Alive";
+    }
 }
