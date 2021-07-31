38893e0ac2b94dbf658208b14244e678c38b7b3d
Ronak Sakhuja <rosakhuj@microsoft.com>
2021-07-31 09:32:00 +0000

    Update HealthCheckApi.java

--- storage-core/src/main/java/org/opengroup/osdu/storage/api/HealthCheckApi.java
+++ storage-core/src/main/java/org/opengroup/osdu/storage/api/HealthCheckApi.java
@@ -1,21 +1,41 @@
 package org.opengroup.osdu.storage.api;
 
+import org.opengroup.osdu.core.common.http.HttpResponse;
+import org.opengroup.osdu.core.common.http.IHttpClient;
+import org.opengroup.osdu.core.common.model.storage.StorageRole;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.opengroup.osdu.core.common.model.storage.StorageRole;
+import org.opengroup.osdu.core.common.http.HttpRequest;
+
+import java.util.Random;
 
 @RestController
 public class HealthCheckApi {
 
+    @Autowired
+    IHttpClient httpClient;
+
     @GetMapping("/health")
-    @PreAuthorize("@authorizationFilter.hasRole('" + StorageRole.ADMIN + "')")
     public String healthMessage() {
-        return "Alive";
+
+        HttpRequest httpRequest1 = HttpRequest.builder().url("http://entitlements/api/entitlements/v2/actuator/chaosmonkey").build();
+        HttpRequest httpRequest2 = HttpRequest.builder().url("http://entitlementsxa/api/entitlements/v2/actuator/chaosmonkey").build();
+        Random rd = new Random();
+        HttpResponse httpResponse=null;
+        if(rd.nextBoolean()){
+            httpResponse =  httpClient.send(httpRequest1);
+        }
+        else {
+            httpResponse = httpClient.send(httpRequest2);
+        }
+        return httpResponse.getBody();
     }
 
     @GetMapping("/healthh")
-    public String healthMessagex2() {
+    @PreAuthorize("@authorizationFilter.hasRole('" + StorageRole.ADMIN + "')")
+    public String healthMessagee() {
         return "Alive";
     }
 }
