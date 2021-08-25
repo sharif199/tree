fe75d4f00a7dfc6e994e8e65b1c08780b97a972e
zhijie wang <wanzhiji@amazon.com>
2021-08-25 10:34:05 -0700

    uodate

--- provider/storage-aws/src/main/java/org/opengroup/osdu/storage/provider/aws/security/UserAccessService.java
+++ provider/storage-aws/src/main/java/org/opengroup/osdu/storage/provider/aws/security/UserAccessService.java
@@ -30,7 +30,7 @@ import org.opengroup.osdu.core.common.model.http.AppException;
 import org.opengroup.osdu.core.common.model.http.DpsHeaders;
 import org.opengroup.osdu.core.common.model.storage.RecordProcessing;
 import org.opengroup.osdu.core.common.util.IServiceAccountJwtClient;
-import org.opengroup.osdu.core.aws.cache.CacheHelper;
+import org.opengroup.osdu.storage.provider.aws.util.CacheHelper;
 import org.opengroup.osdu.storage.service.IEntitlementsExtensionService;
 import org.springframework.stereotype.Service;
 
