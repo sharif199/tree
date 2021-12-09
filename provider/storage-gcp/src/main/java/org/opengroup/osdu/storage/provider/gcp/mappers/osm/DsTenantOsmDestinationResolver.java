f90f51291e2906005a3f72a225273d229e3ffed6
Rostislav_Dublin <Rostislav_Dublin@epam.com>
2021-12-09 11:55:01 +0300

    author labels removed. EPAM license added

--- provider/storage-gcp/src/main/java/org/opengroup/osdu/storage/provider/gcp/mappers/osm/DsTenantOsmDestinationResolver.java
+++ provider/storage-gcp/src/main/java/org/opengroup/osdu/storage/provider/gcp/mappers/osm/DsTenantOsmDestinationResolver.java
@@ -1,3 +1,19 @@
+/*
+  Copyright 2021 Google LLC
+  Copyright 2021 EPAM Systems, Inc
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+ */
 package org.opengroup.osdu.storage.provider.gcp.mappers.osm;
 
 import com.google.api.gax.retrying.RetrySettings;
@@ -24,9 +40,6 @@ import static org.springframework.beans.factory.config.BeanDefinition.SCOPE_SING
 
 /**
  * Resolves Destination.partitionId into info needed by Datastore to address requests to a relevant GCP data project.
- *
- * @author Rostislav_Dublin
- * @since 15.09.2021
  */
 @Component
 @Scope(SCOPE_SINGLETON)
--- provider/storage-gcp/src/main/java/org/opengroup/osdu/storage/provider/gcp/mappers/osm/PgTenantOsmDestinationResolver.java
+++ provider/storage-gcp/src/main/java/org/opengroup/osdu/storage/provider/gcp/mappers/osm/PgTenantOsmDestinationResolver.java
@@ -1,3 +1,19 @@
+/*
+  Copyright 2021 Google LLC
+  Copyright 2021 EPAM Systems, Inc
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+ */
 package org.opengroup.osdu.storage.provider.gcp.mappers.osm;
 
 import com.zaxxer.hikari.HikariDataSource;
@@ -26,9 +42,6 @@ import static org.springframework.beans.factory.config.BeanDefinition.SCOPE_SING
 
 /**
  * Resolves Destination.partitionId into info needed by Postgres to address requests to a relevant DB server.
- *
- * @author Rostislav_Dublin
- * @since 15.09.2021
  */
 @Component
 @Scope(SCOPE_SINGLETON)
